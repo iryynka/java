f3c6f79c2a9f59d1eb1b3c00e626c05e8d700f7e
iryynka <kmet.ira04@gmail.com>
2022-04-27 23:44:41 +0300

    Initial commit

---
+++ src/lesson2/PrimitiveTypes.java
@@ -0,0 +1,22 @@
+package lesson2;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        //Ініціалізація сторін a, b, c; об'єму та периметру паралелепіпеда
+        double a = 4.5d;
+        double b = 6.d;
+        double c = 0.5d;
+        double volume = a * b * c;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        double length = 4*(a+b+c);
+        System.out.println("Периметр паралелепіпеда = " + length);
+
+
+        //Змінні з широтою і довготою гори, виведення в консоль
+        double latitude = 27.987988;
+        double longitude = 86.925168;
+        System.out.println("Координати гори Еверест: \n- широта: " + latitude + "\n- довгота: " + longitude);
+
+    }
+}
