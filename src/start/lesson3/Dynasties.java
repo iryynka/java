6eb6c0f4a31c48750c1f1801523f6075134b9566
iryynka <kmet.ira04@gmail.com>
2022-05-01 20:30:46 +0300

    With \t

--- src/start/lesson3/Dynasties.java
+++ src/start/lesson3/Dynasties.java
@@ -1,20 +1,22 @@
 package start.lesson3;
 
-public class Dinasties {
+public class Dynasties {
     public static void main(String[] args) {
-        //There are 2 dinasties: Li and Ming
+        //There are 2 dynasties: Li and Ming
 
         //Li power per one person
         int warriorLi = 13;
         int archerLi = 24;
         int horsemanLi = 46;
-        System.out.println("Li army (per person): - warrior " + warriorLi + " XPs\n                      - aecher " + archerLi + " XPs\n                      - horseman " + horsemanLi + " XPs");
+        System.out.printf("Li army (per person): \t - warrior %d XPs\n\t\t\t\t\t\t - archer %d XPs\n\t\t\t\t\t\t - horseman %d XPs", warriorLi, archerLi, horsemanLi);
+        System.out.println();
 
         //Ming power per one person
         int warriorMing = 9;
         int archerMing = 35;
         int horsemanMing = 12;
-        System.out.println("Ming army (per person): - warrior " + warriorMing + " XPs\n                        - aecher " + archerMing + " XPs\n                        - horseman " + horsemanMing + " XPs");
+        System.out.printf("Ming army (per person):  - warrior %d XPs\n\t\t\t\t\t\t - arWithcher %d XPs\n\t\t\t\t\t\t - horseman %d XPs", warriorMing, archerMing, horsemanMing);
+        System.out.println();
 
         //The number of each type of soldiers
         int numLi = 860;
