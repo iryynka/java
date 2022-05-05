01448c27e5305dd94bb1645df1d7d197771e0f7b
iryynka <kmet.ira04@gmail.com>
2022-05-05 18:38:23 +0300

    +annotation

--- src/start/lesson4/AnimalsInTenYears.java
+++ src/start/lesson4/AnimalsInTenYears.java
@@ -12,6 +12,17 @@ public class AnimalsInTenYears {
         System.out.println("There will be " + animalsInTenYears + " animals in " + years + " years.");
     }
 
+    /**
+     * Add the amount of animals in n years
+     *
+     * @param bears     the number of bears in y year
+     * @param tigers    the number of tigers in y year
+     * @param parrots   the number of parrots in y year
+     * @param elephants the number of elephants in y year
+     * @param raccoons  the number of raccoons in y year
+     * @param years     the number of year
+     * @return the sum of animals in n years
+     */
     public static int countAnimalsNextYear(int bears, int tigers, int parrots, int elephants, int raccoons, int years) {
         if (years > 0) {
             int animalsThisYear = countAnimalsThisYear(bears, tigers, parrots, elephants, raccoons);
@@ -25,6 +36,16 @@ public class AnimalsInTenYears {
         return 0;
     }
 
+    /**
+     * Add the amount of animals this year
+     *
+     * @param bears     the number of bears this year
+     * @param tigers    the number of tigers this year
+     * @param parrots   the number of parrots this year
+     * @param elephants the number of elephants this year
+     * @param raccoons  the number of raccoons this year
+     * @return the sum of animals this year
+     */
     public static int countAnimalsThisYear(int bears, int tigers, int parrots, int elephants, int raccoons) {
         return bears + tigers + parrots + elephants + raccoons;
     }
